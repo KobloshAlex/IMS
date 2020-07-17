d219ecd55a215a253e077aec36e9a6a60c27ac17
KobloshAlex <Koblosh.Alex@gmail.com>
2020-07-17 14:51:58 -0400

    clean

--- IMS-angular-client/src/app/component/manager/read-manager/read-manager.component.ts
+++ IMS-angular-client/src/app/component/manager/read-manager/read-manager.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {Manager} from '../manager';
 import {ManagerService} from '../../../service/manager.service';
